47d32f19dc8a6f9313283ca40708352667c0a811
Daniel Pierce <dlpierce@indiana.edu>
2018-11-16 11:00:38 -0500

    Spawn subprocess, add start script

--- forklift.py
+++ forklift.py
@@ -1,5 +1,5 @@
 from bottle import get, post, request, abort, run
-from os import system
+from os import spawnl
 
 # Must get provided in request (i.e. /hook?apikey=12345678)
 apikey = '1234'
@@ -18,11 +18,11 @@ def hook():
     params = request.json or abort(400, 'Params not found')
     params['push_data']['tag'] == 'master' or abort(304, 'Not modified')
     container = valid_containers[params['repository']['repo_name']] or abort(404, 'Valid container not found')
-    restart(container) == 0 or abort(500, 'Restart failed')
+    restart(container) or abort(500, 'Restart failed')
     return 'OK'
 
 def restart(container_name):
     cmd = "{}/{}/restart".format(docker_root, container_name)
-    system(cmd)
+    return spawnl(os.P_NOWAIT, cmd)
 
 run(host='0.0.0.0', port=8000, debug=False, reloader=False)
