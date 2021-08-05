039c4ef09bb35e69e3824c5cf46f9d957fed7235
Daniel Pierce <dlpierce@indiana.edu>
2021-08-05 10:35:27 -0400

    Require Python 3, use config file and validate callback urls.

--- forklift.py
+++ forklift.py
@@ -1,13 +1,12 @@
+import bottle
+import sys, os, subprocess, urllib.request
 from bottle import get, post, request, abort, run
 from json import dumps
-import os
+from ast import literal_eval
 
-# Must get provided in request (i.e. /hook?apikey=12345678)
-apikey = '1234'
-# Absolute path to docker containers
-docker_root = '/srv/docker'
-# Map of Docker Hub repo names and directory names
-valid_containers = {'namespace/name':'docker_dir'}
+sys.version_info.major >= 3 or sys.exit('ERROR: Python 3 required')
+app = bottle.default_app()
+app.config.load_config('./forklift.config')
 
 @get('/status')
 def status():
@@ -15,16 +14,31 @@ def status():
 
 @post('/hook')
 def hook():
-    request.params.apikey == apikey or abort(403, 'Forbidden')
-    print 'Request params: ' + dumps(request.params.dict)
+    request.params.apikey == app.config['forklift.api_key'] or abort(403, 'Forbidden')
+    print("Request params: {}".format(dumps(request.params.dict)))
     params = request.json or abort(400, 'Params not found')
     params['push_data']['tag'] == 'master' or abort(304, 'Not modified')
-    container = valid_containers[params['repository']['repo_name']] or abort(404, 'Valid container not found')
-    restart(container) or abort(500, 'Restart failed')
-    return 'OK'
+    repo_name = params['repository']['repo_name']
+    container = validate_container(repo_name)
+    container or abort(404, "Valid container {}:{} not found".format(repo_name, container))
+    validate_webhook(params.get('callback_url'), 'success')
+    output = restart(container) or abort(500, 'Restart failed')
+    return "OK\n{}".format(output)
+
+def validate_container(key):
+    return literal_eval(app.config['forklift.valid_containers'])[key]
+
+def validate_webhook(url, state):
+    if url == None: return
+    data = dumps({'state':state}).encode()
+    req = urllib.request.Request(url=url, data=data, method='POST')
+    print("Validating webhook: {} => {}".format(state, url))
+    return urllib.request.urlopen(req)
 
 def restart(container_name):
-    cmd = "{}/{}/restart".format(docker_root, container_name)
-    return os.spawnl(os.P_NOWAIT, cmd)
+    cmd = "{}/{}/bin/update".format(app.config['forklift.docker_root'], container_name)
+    print("Running command: {}".format(cmd))
+    return subprocess.check_output(cmd)
+    # return os.spawnl(os.P_NOWAIT, cmd)
 
-run(host='0.0.0.0', port=8000, debug=False, reloader=False)
+run(host='0.0.0.0', port=app.config['forklift.port'], debug=False, reloader=False)
